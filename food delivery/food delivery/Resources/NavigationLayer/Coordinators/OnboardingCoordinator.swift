81b215918bedd28dbc94a77a67ddd8b3d1642274
babyshkaBoy <azamatbaev19@yandex.ru>
2024-03-16 17:17:41 +0300

    Added onboarding coordinator

---
+++ food delivery/food delivery/Resources/NavigationLayer/Coordinators/OnboardingCoordinator.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingCoordinator.swift
+//  food delivery
+//
+//  Created by Азамат Баев on 16.03.2024.
+//
+
+import UIKit
+
+class OnboardingCoordinator: Coordinator {
+    override func start() {
+        let vc = ViewController()
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    override func finish() {
+        print("AppCoordinator finish")
+    }
+}
--- food delivery/food delivery/SceneDelegate.swift
+++ food delivery/food delivery/SceneDelegate.swift
@@ -10,17 +10,18 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    var coordinator: AppCoordinator?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        let viewController = ViewController()
-        let navigaionController = UINavigationController(rootViewController: viewController)
+        let navigaionController = UINavigationController()
         window.rootViewController = navigaionController
         self.window = window
         window.makeKeyAndVisible()
+        let appCoordinator = AppCoordinator(type: .app, navigationController: navigaionController)
+        appCoordinator.start()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
